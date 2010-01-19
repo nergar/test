a63031e16a97515a039aa2f2038cf370160eb668
Koko <koko@koko.com>
2010-01-19 14:55:44 +0100

    Added lib

---
+++ lib/simplegit.rb
@@ -0,0 +1,23 @@
+# a super simple example class to use git in ruby
+class SimpleGit
+  
+  def initialize(git_dir = '.')
+    @git_dir = File.expand_path(git_dir)
+  end
+  
+  def show(treeish = 'master')
+    command("git show #{treeish}")
+  end
+
+  private
+  
+    def command(git_cmd)
+      Dir.chdir(@git_dir) do
+        return `#{git_cmd} 2>&1`.chomp
+      end
+    end
+ 
+    def another
+	puts "another"
+    end 
+end
